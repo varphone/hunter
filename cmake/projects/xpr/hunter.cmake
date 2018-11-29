2d18f07b8817fd4fcdb7a68818eff59e2d18a9a7
Varphone Wong <varphone@qq.com>
2018-11-29 16:34:20 +0800

    xpr: new package
    Signed-off-by: Varphone Wong <varphone@qq.com>

---
+++ cmake/projects/xpr/hunter.cmake
@@ -0,0 +1,34 @@
+﻿# Copyright (c) 2014-2015, Ruslan Baratov
+# Copyright (c) 2015, David Hirvonen
+# Copyright (c) 2018, Varphone Wong
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    xpr
+    VERSION
+    "2.1.9-p1"
+    URL
+    "https://github.com/varphone/xpr/archive/v2.1.9-p1.tar.gz"
+    SHA1
+    407b3c13dfa3a8594ebd0a6d2bd2deffd778c0a7
+)
+
+hunter_cmake_args(xpr
+    CMAKE_ARGS
+        BUILD_EXAMPLES=OFF
+        BUILD_TESTS=OFF
+        BUILD_WITH_HUNTER=ON
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(xpr)
+hunter_download(PACKAGE_NAME xpr)
